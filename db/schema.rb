ae80240c001483df17c5a9025e48ea25164a6bf8
eyademad1 <eyademad282@gmail.com>
2026-04-18 07:59:30 +0200

    feat: add address column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_18_055432) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_18_055819) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_18_055432) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "address", null: false
     t.datetime "created_at", null: false
     t.date "dob"
     t.string "email"
