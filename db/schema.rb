18f71a59029ba5f056b7a06124c4db7e9020cfb4
eyademad1 <eyademad282@gmail.com>
2026-04-18 07:53:55 +0200

    feat: add email column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_17_105419) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_18_051925) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_17_105419) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.date "dob"
+    t.string "email"
     t.string "name"
     t.integer "phone"
     t.datetime "updated_at", null: false
