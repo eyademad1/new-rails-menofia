4ebd0aa4daeccea471c9f8e455b8fae633315b27
eyademad1 <eyademad282@gmail.com>
2026-04-18 07:16:26 +0200

    feat: add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    validates :name, :dob, :email, :phone_number, presence: true
+
+    validates :email, uniqueness: true
+
+    validates :email, format: {
+    with: URI::MailTo::EMAIL_REGEXP,
+    message: "must be a valid email"
+    }
 end
